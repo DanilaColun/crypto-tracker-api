1ce107282cd888bc3276814caa1868677a1e65fc
DanilaColun <colun.danila@gmail.com>
2026-06-15 17:14:11 +0300

    used middleware in app

--- src/http/createApp.ts
+++ src/http/createApp.ts
@@ -1,10 +1,23 @@
 import express from 'express';
+import { Logger } from '../logger/logger';
+import { createRequestIdMiddleware } from './middlewares/requestIdMiddleware';
+import { createRequestLoggerMiddleware } from './middlewares/requestLoggerMiddleware';
+import { createErrorMiddleware } from './middlewares/errorMiddleware';
 import { createStatusRoutes } from './routes/statusRoutes';
 
-export function createApp() {
+interface CreateAppOptions {
+  logger: Logger;
+}
+
+export function createApp(options: CreateAppOptions) {
   const app = express();
 
+  app.use(createRequestIdMiddleware());
+  app.use(createRequestLoggerMiddleware({ logger: options.logger }));
+
   app.use(createStatusRoutes());
+
+  app.use(createErrorMiddleware({ logger: options.logger }));
 
   return app;
 }
